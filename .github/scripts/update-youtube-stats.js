ba5ba5fed5c0672fe3b870c128e4b0ac2b7964b1
Dhanush <dhanushxenocrate@gmail.com>
2026-08-31 13:17:18 +0530

    fix: youtube live updates

--- .github/scripts/update-youtube-stats.js
+++ .github/scripts/update-youtube-stats.js
@@ -54,10 +54,10 @@ async function updateStats() {
 function formatNumber(numStr) {
     const num = parseInt(numStr, 10);
     if (num >= 1000000) {
-        return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+        return (num / 1000000).toFixed(2).replace(/\.00$/, '') + 'M';
     }
     if (num >= 1000) {
-        return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+        return (num / 1000).toFixed(2).replace(/\.00$/, '') + 'K';
     }
     return num.toString();
 }
